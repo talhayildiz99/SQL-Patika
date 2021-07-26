e9f75d8d4407648f65f49d993eee591f90b8184f
talhayildiz99 <84921617+talhayildiz99@users.noreply.github.com>
2021-07-26 12:13:48 +0300

    Update PatikaSQL-1.sql

--- PatikaSQL-1.sql
+++ PatikaSQL-1.sql
@@ -1,14 +1,14 @@
--- Query-1 
+-- Query-1 film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
 SELECT title,description FROM film;
---Query-2
+--Query-2 film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
 SELECT * FROM film
 WHERE length > 60 AND length < 75;
---Query-3
+--Query-3 film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
 SELECT * FROM film
 WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99 ;
---QUERY-4
+--QUERY-4 customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
 SELECT last_name FROM customer
 WHERE first_name = 'Mary';
---QUERY-5
+--QUERY-5 film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
  SELECT * FROM film
 WHERE not length > 50 and not (rental_rate = 2.99 or rental_rate = 4.99);
